2eddc910f51dd9765c68934b7d34ca3b3d33f661
Gregory Lustiber <glustiber@gmail.com>
2021-09-07 14:02:18 -0600

    Handled Loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,9 +33,11 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchMeals = async () => {
+            setIsLoading(true);
             const response = await fetch('https://react-http-37327-default-rtdb.firebaseio.com/meals.json');
             const responseData = await response.json();
 
@@ -51,9 +53,18 @@ const AvailableMeals = () => {
             }
 
             setMeals(loadedMeals);
+            setIsLoading(false);
         };
         fetchMeals();
     }, [])
+
+    if (isLoading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        );
+    }
 
     const mealsList = meals.map((meal) => (
         <MealItem 
