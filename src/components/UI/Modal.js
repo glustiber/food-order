b8352603347f3ecb067bca6d53ba7f2dcee7b864
Gregory Lustiber <glustiber@gmail.com>
2021-09-06 21:36:49 -0600

    Move meals data to backend. Fetch meals via HTTP

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,11 +2,11 @@ import {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
-const Backdrop = props => {
+const Backdrop = (props) => {
     return <div className={classes.backdrop} onClick={props.onClose}/>
 };
 
-const ModalOverlay = props => {
+const ModalOverlay = (props) => {
     return (
         <div className={classes.modal}>
             <div className={classes.content}>{props.children}</div>
@@ -16,7 +16,7 @@ const ModalOverlay = props => {
 
 const portalElement = document.getElementById('overlays');
 
-const Modal = props => {
+const Modal = (props) => {
     return (
         <Fragment>
             {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
